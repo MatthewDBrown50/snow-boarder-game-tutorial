28096fe1ce0326819d7786dbbf03e93e3dc0d59e
MatthewDBrown50 <matthew.d.brown50@gmail.com>
2023-05-08 08:06:17 -0400

    Added player controls

---
+++ snow-boarder-game-tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float torque = 100;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+   
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(torque * Time.deltaTime);
+        } 
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(-torque * Time.deltaTime);
+        }
+    }
+}
