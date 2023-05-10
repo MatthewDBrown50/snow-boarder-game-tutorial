f42dfe9f1add1e52742e33483300c48f729b7cf8
MatthewDBrown50 <matthew.d.brown50@gmail.com>
2023-05-09 21:36:52 -0400

    Added particle effect to snowboard and moved boost logic to PlayerController script.

--- snow-boarder-game-tutorial/Assets/Scripts/PlayerController.cs
+++ snow-boarder-game-tutorial/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,47 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
     [SerializeField] private float torque = 100;
 
-    void Start()
+    SurfaceEffector2D se;
+    private float baseSpeed = 10f;
+    private float boostSpeed = 25f;
+
+    private void Start()
     {
+        se = FindObjectOfType<SurfaceEffector2D>();
+        se.speed = baseSpeed;
         rb = GetComponent<Rigidbody2D>();
     }
    
-    void Update()
+    private void Update()
+    {
+        RotatePlayer();
+        AdjustSpeed();
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddTorque(torque * Time.deltaTime);
-        } 
+        }
         else if (Input.GetKey(KeyCode.D))
         {
             rb.AddTorque(-torque * Time.deltaTime);
+        }
+    }
+
+    private void AdjustSpeed()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            se.speed = boostSpeed;
+        }
+        else
+        {
+            se.speed = baseSpeed;
         }
     }
 }
